d31bf164f99612c7d772d4df04e1d068e0e7a5f8
momohari <isnangja@gmail.com>
2022-03-31 15:02:29 +0900

    feat: add rough skeleton for manager

--- E-Project/E-Project/main.cpp
+++ E-Project/E-Project/main.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <fstream>
 
+#define DBG 1
+
 using namespace std;
 
 ifstream readFile;
@@ -17,16 +19,61 @@ public:
     string certi; //CERTI
 };
 
+void add(char* buf) {
+    printf("%s(line:%d)\n", __func__, __LINE__);
+}
+
+void del(char* buf) {
+    printf("%s(line:%d)\n", __func__, __LINE__);
+}
+
+void sch(char* buf) {
+    printf("%s(line:%d)\n", __func__, __LINE__);
+}
+
+void mod(char* buf) {
+    printf("%s(line:%d)\n", __func__, __LINE__);
+}
+
+void manager(char* buf) {
+    if (buf[0] == 'A' && buf[1] == 'D' && buf[2] == 'D') {
+        /* ADD */
+        add(buf);
+    }
+    else if (buf[0] == 'D' && buf[1] == 'E' && buf[2] == 'L') {
+        /* DEL */
+        del(buf);
+    }
+    else if (buf[0] == 'S' && buf[1] == 'C' && buf[2] == 'H') {
+        /* SCH */
+        sch(buf);
+    }
+    else if (buf[0] == 'M' && buf[1] == 'O' && buf[2] == 'D') {
+        /* MOD */
+        mod(buf);
+    }
+}
+
 int main(int argc, char* argv[])
 {
-    string inputFileName = argv[1];
-    string outputFileName = argv[2];
+    string inputFileName;
+    string outputFileName;
+    if (DBG) {
+        inputFileName = "input.txt";
+        outputFileName = "output.txt";
+    }
+    else {
+        inputFileName = argv[1];
+        outputFileName = argv[2];
+    }
     readFile.open(inputFileName);
     printFile.open(outputFileName);
     if (readFile.is_open()) {
         while (!readFile.eof()) {
             char buf[256] = { 0, };
             readFile.getline(buf, 256);
+            manager(buf);
+
             printFile << buf << endl;
         }
     }
