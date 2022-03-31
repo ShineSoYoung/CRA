4b45ffa32d8b93c4746c822d012601c1cfe6135e
BulgogiBake <102651378+BulgogiBake@users.noreply.github.com>
2022-03-31 12:47:53 +0900

    feat: auto save to utf8 for Korean

--- E-Project/E-Project/main.cpp
+++ E-Project/E-Project/main.cpp
@@ -1,13 +1,17 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <iostream>
 
 using namespace std;
-  
+
+/*
+* 한글 주석 테스트입니다
+*/
 int main()
 {
     cout << "HelloWorld\n";
     printf("hello world!\n");
     printf("hello world! ssy\n");
+    cout << "안녕하세요. 한글테스트입니다.\n";
 
     return 0;
 }
