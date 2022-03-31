79399b84bdbb06ffd47d40ad995601c765f8f7a9
momohari <isnangja@gmail.com>
2022-03-31 14:07:06 +0900

    Feat: COM: Add file input/output

--- E-Project/E-Project/main.cpp
+++ E-Project/E-Project/main.cpp
@@ -1,23 +1,35 @@
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
 
 using namespace std;
-  
+
+ifstream readFile;
+ofstream printFile;
+
 class Employee {
 public:
-    string employeeNum;
-    string name;
-    string cl;
-    string phoneNum;
-    string birthday;
-    string certi;
+    string employeeNum; //사원번호
+    string name; //성명
+    string cl; //경력개발단계
+    string phoneNum; //전화번호
+    string birthday; //생년월일
+    string certi; //CERTI
 };
 
-int main()
+int main(int argc, char* argv[])
 {
-    cout << "HelloWorld\n";
-    printf("hello world!\n");
-    printf("hello world! ssy\n");
+    string inputFileName = argv[1];
+    string outputFileName = argv[2];
+    readFile.open(inputFileName);
+    printFile.open(outputFileName);
+    if (readFile.is_open()) {
+        while (!readFile.eof()) {
+            char buf[256] = { 0, };
+            readFile.getline(buf, 256);
+            printFile << buf << endl;
+        }
+    }
 
     return 0;
 }
