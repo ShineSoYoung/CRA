541972c74cdfe4c5ec1f53a8e8ac12351179d4d1
sw.hyun <hsu9643@gmail.com>
2022-04-01 16:36:54 +0900

    test: add test SCN about Mod Function with option2
    - 옵션 1을 제외한 mod Function TEST
    
    Signed-off-by: sw.hyun <hsu9643@gmail.com>

--- E-Project/TEST/test.cpp
+++ E-Project/TEST/test.cpp
@@ -52,8 +52,12 @@ public:
 protected:
     virtual void SetUp() override
     {
-        list.push_back(new Employee{ "18050301","KYUMOK KIM","CL2","010-9777-6055","19980906","PRO" });
-        list.push_back(new Employee{ "18050302","SEUNGWOO HYUN","CL3","010-1111-2233","19900302","PRO" });
+        list.push_back(new Employee{ "18050302", "KYUMOK KIM","CL2","010-9777-6055","19980906","PRO" });
+        list.push_back(new Employee{ "18050301", "NICE JIN","CL3","010-1111-2233","19990506","PRO" });
+        list.push_back(new Employee{ "19000000", "SAM CHOI","CL1","010-3333-1111","19990506","PRO" });
+        list.push_back(new Employee{ "18011111", "CHOI BUS","CL4","010-5555-1111","19990508","PRO" });
+        list.push_back(new Employee{ "18022222", "TAXI CHOI","CL3","010-6666-1111","19990408","PRO" });
+        list.push_back(new Employee{ "18000000", "HARBANG KIM","CL2","010-3333-1111","19990208","PRO" });
     }
 
     virtual void TearDown() override
@@ -68,17 +72,101 @@ protected:
     modifyCommand modCmd;
 };
 
-TEST_F(ModTest, mod_with_name) {
-    EXPECT_EQ(list.size(), 2);
-    EXPECT_EQ(list[0]->name, "KYUMOK KIM");
-    EXPECT_EQ(list[1]->name, "SEUNGWOO HYUN");
+TEST_F(ModTest, mod_with_name_none_none) {
+    EXPECT_EQ(list.size(), 6);
 
-    string result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,false,false,false,{"employeeNum", "18050301", "phoneNum", "010-0000-0000"}});
+    string result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,false,false,false,{"cl", "CL2", "name", "CHANGE CHANGE"} });
+    EXPECT_EQ(result, "MOD,2\n");
+
+    result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,false,false,false,{"name", "TAXI CHOI", "certi", "EX"} });
     EXPECT_EQ(result, "MOD,1\n");
 
-    result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,false,false,false,{"name", "SEUNGWOO HYUN", "certi", "EX"} });
+    result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,false,false,false,{"certi", "PRO", "phoneNum", "010-0000-0000"} });
+    EXPECT_EQ(result, "MOD,5\n");
+}
+TEST_F(ModTest, mod_with_cl_none_none) {
+    string result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,false,false,false,{"name", "HARBANG KIM", "cl", "CL4"} });
+    EXPECT_EQ(result, "MOD,1\n");
+
+    result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,false,false,false,{"cl", "CL3", "phoneNum", "010-9999-9999"} });
+    EXPECT_EQ(result, "MOD,2\n");
+}
+TEST_F(ModTest, mod_with_phoneNum_none_none) {
+    string result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,false,false,false,{"name", "HARBANG KIM", "cl", "CL4"} });
+    EXPECT_EQ(result, "MOD,1\n");
+
+    result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,false,false,false,{"cl", "CL3", "phoneNum", "010-9999-9999"} });
+    EXPECT_EQ(result, "MOD,2\n");
+
+}
+TEST_F(ModTest, mod_with_birthday_none_none) {
+    string result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,false,false,false,{"birthday", "19990506", "cl", "CL2"} });
+    EXPECT_EQ(result, "MOD,2\n");
+
+    result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,false,false,false,{"name", "NICE JIN", "birthday", "19990208"} });
     EXPECT_EQ(result, "MOD,1\n");
 }
+
+TEST_F(ModTest, mod_with_name_none_f) {
+    string result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,true,false,false,false,false,false,false,{"name", "TAXI", "name", "HARBANG HYUN"} });
+    EXPECT_EQ(result, "MOD,1\n");
+
+    result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,true,false,false,false,false,false,false,{"name", "HARBANG", "birthday", "19990506"} });
+    EXPECT_EQ(result, "MOD,2\n");
+}
+
+TEST_F(ModTest, mod_with_phoneNum_none_m) {
+    string result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,true,false,false,false,false,{"phoneNum", "3333", "phoneNum", "010-1111-1111"} });
+    EXPECT_EQ(result, "MOD,2\n");
+
+    result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,true,false,false,false,false,{"phoneNum", "1111", "birthday", "19990506"} });
+    EXPECT_EQ(result, "MOD,3\n");
+
+    result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,true,false,false,false,false,{"phoneNum", "3333", "birthday", "19990506"} });
+    EXPECT_EQ(result, "MOD,NONE\n");
+}
+
+TEST_F(ModTest, mod_with_birthday_none_y) {
+    string result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,true,false,false,{"birthday", "1998", "phoneNum", "010-1111-1111"} });
+    EXPECT_EQ(result, "MOD,1\n");
+
+    result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,true,false,false,{"birthday", "1999", "birthday", "19940101"} });
+    EXPECT_EQ(result, "MOD,5\n");
+
+    result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,true,false,false,{"birthday", "1999", "birthday", "19940101"} });
+    EXPECT_EQ(result, "MOD,NONE\n");
+}
+TEST_F(ModTest, mod_with_birthday_none_m) {
+    string result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,false,true,false,{"birthday", "05", "birthday", "19990408"} });
+    EXPECT_EQ(result, "MOD,3\n");
+
+    result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,false,true,false,{"birthday", "04", "birthday", "19941001"} });
+    EXPECT_EQ(result, "MOD,4\n");
+
+    result = modCmd.modify_employee(list, CmdParam{ CmdType::MOD,false,false,false,false,false,false,true,false,{"birthday", "04", "name", "ZZ ZZ"} });
+    EXPECT_EQ(result, "MOD,NONE\n");
+}
+
+#if 0
+// [TBD] -p Option Test
+TEST_F(ModTest, mod_with_certi_p_none) {
+
+}
+
+TEST_F(ModTest, mod_with_name_p_l) {
+
+}
+
+TEST_F(ModTest, mod_with_phoneNum_p_m) {
+
+}
+TEST_F(ModTest, mod_with_phoneNum_p_l) {
+
+}
+TEST_F(ModTest, mod_with_birthday_p_d) {
+
+}
+#endif
 
 
 class DeleteTest : public ::testing::Test
