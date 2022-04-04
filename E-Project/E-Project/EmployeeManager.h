9a2afcb862dc063e8a6a029bce498dcaa0b3de98
momohari <isnangja@gmail.com>
2022-04-04 11:56:21 +0900

    feat: Implementation Employee Manager
    Signed-off-by: momohari <isnangja@gmail.com>

---
+++ E-Project/E-Project/EmployeeManager.h
@@ -0,0 +1,48 @@
+﻿#include "ioManager.h"
+#include "command.h"
+#include "addCommand.h"
+#include "deleteCommand.h"
+#include "searchCommand.h"
+#include "modifyCommand.h"
+
+class EmployeeManager {
+public:
+	EmployeeManager() {
+		io = new IoManager();
+	}
+
+	void setInputData(string inputFileName, string outputFileName) {
+		io->openIoFile(inputFileName, outputFileName);
+	}
+
+	void run() {
+		if (io->isInputFileOpen()) {
+			while (!io->isInputFileEnd()) {
+				CmdParam cmdParam = io->getDataByLine();
+
+				switch (cmdParam.cmd) {
+				case CmdType::ADD:
+					command = new addCommand();
+					break;				
+				case CmdType::DEL:
+					command = new deleteCommand();
+					break;				
+				case CmdType::SCH:
+					command = new searchCommand();
+					break;				
+				case CmdType::MOD:
+					command = new modifyCommand();
+					break;
+				}
+
+				string result;
+				result = command->processCommand(cmdParam);
+				io->printString(result);
+			}
+		}
+	}
+
+private:
+	IoManager* io = nullptr;
+	command* command = nullptr;
+};
--- E-Project/E-Project/main.cpp
+++ E-Project/E-Project/main.cpp
@@ -3,71 +3,18 @@
 #include <sstream>
 #include <string>
 
+#include "EmployeeManager.h"
 #include "commandClassType.h"
 #include "ioManager.h"
 
 using namespace std;
 
-vector<string> split(string str, char Delimiter) {
-    istringstream iss(str);
-    string buffer;
-    vector<string> result;
-
-    while (getline(iss, buffer, Delimiter)) {
-        result.push_back(buffer);
-    }
-    return result;
-}
-
-
-void add(CmdParam cmdParam) {
-    printf("%s(line:%d)\n", __func__, __LINE__);
-}
-
-void del(CmdParam cmdParam) {
-    printf("%s(line:%d)\n", __func__, __LINE__);
-}
-
-void sch(CmdParam cmdParam) {
-    
-    printf("%s(line:%d)\n", __func__, __LINE__);
-}
-
-void mod(CmdParam cmdParam) {
-    printf("%s(line:%d)\n", __func__, __LINE__);
-}
-
-void manager(CmdParam cmdParam) {
-    if (cmdParam.cmd == CmdType::ADD) {
-        /* ADD */
-        add(cmdParam);
-    }
-    else if (cmdParam.cmd == CmdType::DEL) {
-        /* DEL */
-        del(cmdParam);
-    }
-    else if (cmdParam.cmd == CmdType::SCH) {
-        /* SCH */
-        sch(cmdParam);
-    }
-    else if (cmdParam.cmd == CmdType::MOD) {
-        /* MOD */
-        mod(cmdParam);
-    }
-}
-
-#if 0
+#if 1
 int main(int argc, char* argv[])
 {
-    IoManager* io = new IoManager();
-    io->openIoFile(argv[1], argv[2]);
-    
-    if (io->isInputFileOpen()) {
-        while (!io->isInputFileEnd()) {
-            CmdParam cmdParam = io->getInput();
-            manager(cmdParam);
-        }
-    }
+    EmployeeManager* employeeManager = new EmployeeManager();
+    employeeManager->setInputData(argv[1], argv[2]);
+    employeeManager->run();
 
     return 0;
 }
